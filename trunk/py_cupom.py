d58f1ffbf864b1b919d3e0b402c86215f2713b0f
paulo.scardine@gmail.com <paulo.scardine@gmail.com@766c5a5f-ec32-d2f7-48e9-3478261e36eb>
2010-09-10 17:17:24 +0000

    Auto length instead of default length=5

--- trunk/py_cupom.py
+++ trunk/py_cupom.py
@@ -28,7 +28,11 @@ notation that has the following attributes:
 Maximum range can be calculated as 32 elevated to the number of digits used.
 
 You can customize FW_MAP and BW_MAP to make codes harder to guess, but this
-algorithm was not designed to be crypto safe.
+algorithm was not designed to be crypto safe. I made my map I made 0=A because
+I prefer AAAY1 over 000Y1 when padding. This has some side-effects:
+
+   * AAAAA1 == AAA1 == 1 (000001 == 0001 == 1)
+   * checksum calculation also ignores any A(s) on the left
 
 Sorry, guys, only Python >= 2.6 for now. Backporting python 2.6+ bin() is left
 as an exercise to the reader.
@@ -37,7 +41,11 @@ as an exercise to the reader.
 import math
 
 FW_MAP = {
-    '01010':	'0',
+    # 0=A because I prefer AAAY1 over 000Y1 when padding
+    # Side-effects:
+    #   * AAAAA == AAA == A (00000 == 000 == 0)
+    #   * checksum ignores any A(s) on the left side
+    '00000':	'A',
     '00001':	'1',
     '00010':	'2',
     '00011':	'3',
@@ -47,7 +55,7 @@ FW_MAP = {
     '00111':	'7',
     '01000':	'8',
     '01001':	'9',
-    '00000':	'A',
+    '01010':	'0',
     '01011':	'B',
     '01100':	'C',
     '01101':	'D',
@@ -72,7 +80,7 @@ FW_MAP = {
 }
 
 BW_MAP = {
-    '0': '01010', 'O': '01010', 'o': '01010',
+    'A': '00000', 'a': '00000',
     '1': '00001', 'i': '00001', 'I': '00001', 'l': '00001', 'L': '00001',
     '2': '00010',
     '3': '00011',
@@ -82,7 +90,7 @@ BW_MAP = {
     '7': '00111',
     '8': '01000',
     '9': '01001',
-    'A': '00000', 'a': '00000',
+    '0': '01010', 'O': '01010', 'o': '01010',
     'B': '01011', 'b': '01011',
     'C': '01100', 'c': '01100',
     'D': '01101', 'd': '01101',
@@ -155,12 +163,19 @@ Parameters:
     code = ''.join([ FW_MAP[padded_bin[i*5:i*5+5]] for i in range(0, length) ])
     return code + digit(code) if check_digit else code
 
-def decode(s):
+def decode(s, check_digit=False):
     """Decodes an alphanumeric code to integer"""
+    if check_digit:
+        if not check(s):
+            raise ChecksumError("'%s' is invalid cs digit for '%s'" % (s[-1:], s[:-1]))
+        s = s[:-1]
     return int(''.join([BW_MAP[l] for l in str(s)]), 2)
 
-def main():
+class ChecksumError(Exception):
     pass
+
+def main():
+    print """Use 'import py_cupom' inside your code."""
 
 if __name__ == '__main__':
     main()
